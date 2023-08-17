77e7596e03fb75c24019e6246ea850f677fda3d2
zain ali <oral.alizain.123@gmail.com>
2023-08-17 16:38:58 +0500

    added last components

---
+++ secondlayout/src/components/Search.vue
@@ -0,0 +1,30 @@
+<template>
+                <div class="py-32"> 
+                    <div class="max-w-7-xl mx-40 rounded-3xl bg-indigo-50   ">
+                        <div class="flex flex-cols-2">
+                            <div class="  px-4 py-20 text-left">
+                                <p class="text-5xl text-blue-900 ">Stay up to date</p>
+                                <p class="mt-4 text-lg text-blue-900 w-2/4">Get updates on all of our events and be the first to get notified when tickets go on sale. </p>
+                            </div>
+                            <div class="grid py-20">
+                            <h3 class="text-lg font-semibold  text-blue-900">Sign up to our newsletter ↓</h3>
+                            <div class="flex w-72 mr-20 rounded-2xl border-2 border-blue-900 h-16 overflow-hidden">
+                                <input type="email" placeholder="Email address "  class="py-0  ">
+                            
+                                    <button class=" bg-blue-600 text-white hover:bg-blue-500">SignUp Today</button>
+                            
+                            </div>
+                            </div>
+                        
+    
+                        </div>
+                    </div>
+
+                </div>
+</template>
+
+<script>
+export default {
+    name:"SearchComp"
+}
+</script>
--- secondlayout/src/views/LayoutView.vue
+++ secondlayout/src/views/LayoutView.vue
@@ -1,32 +1,35 @@
-<template>
-    <div class="bg-indigo-100/20">
-        
-        <NavBar />
-        <main class="flex-auto ">
-          <BodyText />
-          <Speakers />
-          <Scheduler/>
-          <Sponsers/>
-        </main>
-        <FooterBar />
-    </div>
-</template>
-<script>
-import NavBar from "@/components/NavBar.vue";
-import FooterBar from "@/components/FooterBar.vue";
-import BodyText from "@/components/BodyText.vue";
-import Speakers from "@/components/Speakers.vue";
-import Scheduler from "@/components/Scheduler.vue";
-import Sponsers from "@/components/Sponsers.vue";
-export default {
-  name: "LayoutView",
-  components: {
-    NavBar,
-    FooterBar,
-    BodyText,
-      Speakers,
-      Scheduler,
-      Sponsers
-  },
-};
-</script>
+    <template>
+        <div class="bg-indigo-100/20">
+            
+            <NavBar />
+            <main class="flex-auto ">
+            <BodyText />
+            <Speakers />
+            <Scheduler/>
+            <Sponsers/>
+            <Search/>
+            </main>
+            <FooterBar />
+        </div>
+    </template>
+    <script>
+    import NavBar from "@/components/NavBar.vue";
+    import FooterBar from "@/components/FooterBar.vue";
+    import BodyText from "@/components/BodyText.vue";
+    import Speakers from "@/components/Speakers.vue";
+    import Scheduler from "@/components/Scheduler.vue";
+    import Sponsers from "@/components/Sponsers.vue";
+    import Search from "@/components/Search.vue";
+    export default {
+    name: "LayoutView",
+    components: {
+        NavBar,
+        FooterBar,
+        BodyText,
+        Speakers,
+        Scheduler,
+        Sponsers,
+        Search
+    },
+    };
+    </script>
